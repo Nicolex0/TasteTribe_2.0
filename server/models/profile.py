9e977cdba0d8ade7e2f74907a5e5c59c8ab6c0bd
Xander-AJ <xander.kamau7@gmail.com>
2024-08-12 23:38:58 +0300

    correct file structure

--- server/models/profile.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class Profile(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-    profile_image_url = db.Column(db.String(255))
-    bio = db.Column(db.Text)
