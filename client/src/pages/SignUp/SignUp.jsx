396916d526685cbeb2721155d65b4912ac1b7a5c
tonybrianmwenda <tonybrianmwenda@gmail.com>
2024-08-13 00:29:39 +0300

    Added imports and set states

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { VscLockSmall } from "react-icons/vsc";
+import { TbEyeClosed } from "react-icons/tb";
+import { RiEyeCloseFill } from "react-icons/ri";
+import { auth } from '../../firebase'; // Import auth from your firebase.js
+import { createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'; // Import Firebase methods
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
-  return (
-    <div>
-      SignUp
-    </div>
-  )
-}
-
-export default SignUp
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false); // New state for alert visibility
+  const [verificationEmailSent, setVerificationEmailSent] = useState(false); // New state for email verification status
