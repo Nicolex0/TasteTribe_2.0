7b1b8628cfe997d746f9e6d01557b165eb7c0eb7
Nicole Apono <napononicole@gmail.com>
2024-08-12 23:04:44 +0300

    setting initial state for recipes and recipe operations

--- client/src/pages/UserRecipes/UserRecipes.jsx
+++ client/src/pages/UserRecipes/UserRecipes.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import RecipeCard from '../components/RecipeCard';
 
 const UserRecipes = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [editingRecipe, setEditingRecipe] = useState(null);
+  const [newRecipe, setNewRecipe] = useState({
+    chefImage: '',
+    title: '',
+    chefName: '',
+    image: '',
+    ingredients: '',
+    instructions: '',
+    url: '',
+    moreInfoUrl: '',
+    rating: 0,
+    prepTime: '',
+    servings: 0,
+    countryOfOrigin: '',
+    dietType: '',
+  });
+
   return (
-    <div>
-      My Recipes
-    </div>
+    <div></div>
   )
 }
 
-export default UserRecipes
+export default UserRecipes;
