ddfaaf4da309906faea9841469f464b7d10dd38a
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-08-13 02:27:24 +0300

    Set state to manage form data, form submission status and message

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
 
 const ContactUs = () => {
+  // State to manage form data
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    inquiryType: '',
+    message: '',
+  });
+  console.log(`This is state 1 :${formData}`)
+
+  // State to manage form submission status and message
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [submitMessage, setSubmitMessage] = useState('');
   return (
     <div>
-      ContactUs
+      
     </div>
   )
 }
