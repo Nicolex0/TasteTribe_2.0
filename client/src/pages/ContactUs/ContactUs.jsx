cbc2449550910ae40b966ffa3184ad1c46dc5672
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-08-13 02:51:51 +0300

    Styled the contactus page

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -1,70 +1,94 @@
-import React, { useState } from 'react';
-import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
-
+import React, { useState } from "react";
+import {
+  FaPhoneAlt,
+  FaEnvelope,
+  FaMapMarkerAlt,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
 const ContactUs = () => {
   // State to manage form data
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    inquiryType: '',
-    message: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    inquiryType: "",
+    message: "",
   });
-  console.log(`This is state 1 :${formData}`)
+  console.log(`This is state 1 :${formData}`);
 
   // State to manage form submission status and message
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const [submitMessage, setSubmitMessage] = useState('');
+  const [submitMessage, setSubmitMessage] = useState("");
+
+  // Handle input changes
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  console.log(`This is state 2 :${formData}`);
 
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Create a new object with form data
-    const newFormData = {firstName:formData.firstName, lastName:formData.lastName, email:formData.email, phoneNumber:formData.phoneNumber, inquiryType:formData.inquiryType, message:formData.message }
-    console.log(`This is state 3 :${newFormData}`)
+    const newFormData = {
+      firstName: formData.firstName,
+      lastName: formData.lastName,
+      email: formData.email,
+      phoneNumber: formData.phoneNumber,
+      inquiryType: formData.inquiryType,
+      message: formData.message,
+    };
+    console.log(`This is state 3 :${newFormData}`);
     try {
-        // Send POST request to the server
-        await fetch('http://localhost:8080/contact', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newFormData),
-            
-        })
+      // Send POST request to the server
+      await fetch("http://localhost:8080/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newFormData),
+      })
         .then((r) => r.json())
-        .then((data) => {() => setSubmitMessage(data)});
-
-
-        // Set submission status and reset form
-        setIsSubmitted(true);
-        setFormData({
-            firstName: '',
-            lastName: '',
-            email: '',
-            phoneNumber: '',
-            inquiryType: '',
-            message: '',
+        .then((data) => {
+          () => setSubmitMessage(data);
         });
+
+      // Set submission status and reset form
+      setIsSubmitted(true);
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        inquiryType: "",
+        message: "",
+      });
     } catch (error) {
-        // Handle errors
-        setSubmitMessage('Failed to send message.');
-        console.error('There was an error!', error);
+      // Handle errors
+      setSubmitMessage("Failed to send message.");
+      console.error("There was an error!", error);
     }
-};
+  };
+
   return (
-    <>
     <div className="min-h-screen bg-backgroundGreen flex flex-col items-center p-0 font-urbanist">
       <h1 className="text-4xl font-bold text-center mb-4">Contact Us</h1>
-      <p className="text-center mb-8 font-light">Any question or remarks? Just write us a message!</p>
+      <p className="text-center mb-8 font-light">
+        Any question or remarks? Just write us a message!
+      </p>
       <main className="flex-grow w-full max-w-7xl mx-auto mt-5 mb-2.5 bg-white p-8 shadow-xl">
         <div className="flex space-x-8">
           {/* Contact Information Section */}
           <div className="w-1/2 h-screen bg-customGreen text-white p-8 rounded-xl">
             <h2 className="text-2xl font-bold mb-4">Contact Information</h2>
-            <p className='mb-4'>Say something to start a live chat!</p>
+            <p className="mb-4">Say something to start a live chat!</p>
             <ul className="mt-40 space-y-10">
               <li className="flex items-center space-x-2">
                 <FaPhoneAlt className="text-xl text-white" />
@@ -225,19 +249,14 @@ const ContactUs = () => {
                   Send Message
                 </button>
               </div>
-              </form>
-              {/* Submission Confirmation Message */}
+            </form>
+            {/* Submission Confirmation Message */}
             {isSubmitted ? <p>Your message is sent successfully!</p> : null}
-              </div>          
-            </div>
-            </main>
-            </div>
-            
-            
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-      
-    </>
-  )
-}
-
-export default ContactUs
+export default ContactUs;
