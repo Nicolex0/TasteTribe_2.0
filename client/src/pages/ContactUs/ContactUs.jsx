13ff956eacb4eee7a1b5e6f149bc3270946a4eac
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-08-13 02:29:00 +0300

    Handle form submission and errors

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -17,6 +17,43 @@ const ContactUs = () => {
   // State to manage form submission status and message
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [submitMessage, setSubmitMessage] = useState('');
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Create a new object with form data
+    const newFormData = {firstName:formData.firstName, lastName:formData.lastName, email:formData.email, phoneNumber:formData.phoneNumber, inquiryType:formData.inquiryType, message:formData.message }
+    console.log(`This is state 3 :${newFormData}`)
+    try {
+        // Send POST request to the server
+        await fetch('http://localhost:8080/contact', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newFormData),
+            
+        })
+        .then((r) => r.json())
+        .then((data) => {() => setSubmitMessage(data)});
+
+
+        // Set submission status and reset form
+        setIsSubmitted(true);
+        setFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            phoneNumber: '',
+            inquiryType: '',
+            message: '',
+        });
+    } catch (error) {
+        // Handle errors
+        setSubmitMessage('Failed to send message.');
+        console.error('There was an error!', error);
+    }
+};
   return (
     <div>
       
