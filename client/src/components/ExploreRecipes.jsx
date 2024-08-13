b4693f2deaa589ebc9f0f169a535f6ffdd99d97b
Nicole Apono <napononicole@gmail.com>
2024-08-13 07:38:37 +0300

    resolving merging conflicts

---
+++ client/src/components/ExploreRecipes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ExploreRecipes = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default ExploreRecipes
--- client/src/pages/Recipes/Recipes.jsx
+++ client/src/pages/Recipes/Recipes.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import FeaturedRecipe from '../../components/FeaturedRecipe'
+import ExploreRecipes from '../../components/ExploreRecipes'
 
 const Recipes = () => {
   return (
     <div>
       <FeaturedRecipe/>
+      <ExploreRecipes />
     </div>
   )
 }
