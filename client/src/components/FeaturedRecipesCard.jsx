cf5c802f3c5dd13912504611f011fa2006cbee33
Nicole Apono <napononicole@gmail.com>
2024-08-13 06:51:44 +0300

    Add social media sharing functionality for recipe cards

--- client/src/components/FeaturedRecipesCard.jsx
+++ client/src/components/FeaturedRecipesCard.jsx
@@ -19,4 +19,29 @@ const RecipeCard = ({recipe}) => {
   const handleCommentClick = () => {
     window.location.href = recipe.moreInfoUrl;
   };
+
+  const handleShare = (platform) => {
+    let shareUrl = '';
+    switch (platform) {
+      case 'facebook':
+        shareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(recipe.url)}`;
+        break;
+      case 'twitter':
+        shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(recipe.url)}&text=${encodeURIComponent(recipe.title)}`;
+        break;
+      case 'instagram':
+        // Instagram doesn't have a direct sharing URL, so we'll open Instagram's homepage
+        shareUrl = 'https://www.instagram.com/';
+        break;
+      case 'whatsapp':
+        shareUrl = `https://wa.me/?text=${encodeURIComponent(`Check out this recipe: ${recipe.title} ${recipe.url}`)}`;
+        break;
+      default:
+        break;
+    }
+    if (shareUrl) {
+      window.open(shareUrl, '_blank', 'noopener,noreferrer');
+    }
+  };
+
 }
